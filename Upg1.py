786dff0509c866d448f16a43059e61aa4baf5cbc
amaroka <70513402+amaroka@users.noreply.github.com>
2020-08-31 13:58:56 +0200

    Delete Upg1.py

--- Upg1.py
+++
@@ -1,4 +0,0 @@
-print('Hej, mitt namn är Daniel Yngve, Jag växte upp i Nol som ligger i Ale kommun.')
-print('Har programmerat tidigare i mest när det kommer till webbbaserat/databaser som html,css,php lite javascript under gymnasiet,')
-print('men jag har vid senare tillfälle programmerat lite med c++.')
-print('För utbildningen låter intressant, för jag har ett intresse i teknik/dator.')
