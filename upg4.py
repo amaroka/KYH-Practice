bfa00aa2bb8960211880b1ea0f95d591e1e67f38
Daniel <daniel.yngve@student.kyh.se>
2020-09-01 10:03:58 +0200

    nummer mellan 1 & 100

---
+++ upg4.py
@@ -0,0 +1,22 @@
+import random
+
+n = random.randint(1 ,100)
+print("Jag tänker på ett nummer mellan 1 och 100, Gissa??")
+
+Guess = 0
+
+while True:
+    text = input("Din gissning")
+    as_number = int(text)
+    Guess = Guess+1
+    if as_number == n:
+            print("Korrekt, du gissade så här mycket = " + str(Guess))
+            break
+
+    if as_number < n:
+            print("Mitt nummer är högre... Gissa igen!")
+
+    if as_number > n:
+            print("Mitt nummer är lägre... Gissa igen!")
+
+
