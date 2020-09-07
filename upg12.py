f435b0ac589cbcb2231e313663e264a586d05c67
Daniel <daniel.yngve@student.kyh.se>
2020-09-07 10:40:05 +0200

    Namn lista

---
+++ upg12.py
@@ -0,0 +1,10 @@
+def is_it_too_long(name):
+    return len(name) > 5
+
+def main():
+        students = input("Ange studenternas namn med komma emellan: ").split(',')
+        for name in students:
+                if is_it_too_long(name):
+                    print(f"{name} är för långt!")
+if __name__ == '__main__':
+    main()
