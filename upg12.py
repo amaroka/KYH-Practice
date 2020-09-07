eef1c7a807fc957c1381a263a77d743e395da70a
Daniel <daniel.yngve@student.kyh.se>
2020-09-07 11:18:50 +0200

    Stor bokstav

--- upg12.py
+++ upg12.py
@@ -4,7 +4,7 @@ def is_it_too_long(name):
 def main():
         students = input("Ange studenternas namn med komma emellan: ").split(',')
         for name in students:
-                if is_it_too_long(name):
-                    print(f"{name} är för långt!")
+            if is_it_too_long(name):
+                print(f"{name.title()} är för långt!")
 if __name__ == '__main__':
     main()
