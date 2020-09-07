fd7b2908ec34306fcf88464350021c0a987a5358
Daniel <daniel.yngve@student.kyh.se>
2020-09-07 11:36:02 +0200

    Maxlängd på namn

--- upg12.py
+++ upg12.py
@@ -1,10 +1,14 @@
-def is_it_too_long(name):
-    return len(name) > 5
+def is_it_too_long(name, max_length):
+    return len(name) > max_length
 
 def main():
+        try:
+            max_length = int(input("Ange max längd på namn: "))
+        except ValueError:
+            max_length = 5
         students = input("Ange studenternas namn med komma emellan: ").split(',')
         for name in students:
-            if is_it_too_long(name):
+            if is_it_too_long(name, max_length):
                 print(f"{name.title()} är för långt!")
 if __name__ == '__main__':
     main()
