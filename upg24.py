1aba4097ff0c17a940946355a2149cbd442b4c16
Daniel <daniel.yngve@student.kyh.se>
2020-09-15 14:12:02 +0200

    25.1

--- upg24.py
+++ upg24.py
@@ -1,17 +1,18 @@
-#from pprint import pprint
 import requests
+#import datetime
 
 URL = "https://proagile.se/api/publicEvents"
 r = requests.get(url= URL)
 
 data = r.json()
 
-# today = datetime.datetime.today()
+#today = datetime.datetime.today()
 
 for elements in data:
-    print(f"Kursnamn:    {elements['name']}")
-    print(f"Startdatum:{elements['startDate']:>12}")
-    print(f"Slutdatum:{elements['endDate']:>13}")
+    if elements["startDate"] == "2020-09-15":
+        print(f"Kursnamn:    {elements['name']}")
+        print(f"Startdatum:{elements['startDate']:>12}")
+        print(f"Slutdatum:{elements['endDate']:>13}")
 
 
 
