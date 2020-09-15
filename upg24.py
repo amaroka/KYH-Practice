00ea0f278c924a953c3717cbfe23136dd70106f2
Daniel <daniel.yngve@student.kyh.se>
2020-09-15 14:03:02 +0200

    25.1

--- upg24.py
+++ upg24.py
@@ -6,5 +6,12 @@ r = requests.get(url= URL)
 
 data = r.json()
 
+# today = datetime.datetime.today()
+
 for elements in data:
-    print(elements['startDate'])
+    print(f"Kursnamn:    {elements['name']}")
+    print(f"Startdatum:{elements['startDate']:>12}")
+    print(f"Slutdatum:{elements['endDate']:>13}")
+
+
+
