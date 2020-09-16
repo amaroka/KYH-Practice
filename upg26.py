5a1bd01f8f0ed0b766ab89ae3df6c99917128afe
Daniel <daniel.yngve@student.kyh.se>
2020-09-16 11:39:14 +0200

    26.3

--- upg26.py
+++ upg26.py
@@ -1,6 +1,21 @@
-from pprint import pprint
+#from pprint import pprint
 import requests
 
-r = requests.get("http://www.omdbapi.com/", params={"t": "Alien", "apikey": "9f6d550c"})
 
-pprint(r.json())
+
+film = input("Sök på en film: ")
+r = requests.get("http://www.omdbapi.com/", params={"t": film, "apikey": "9f6d550c"})
+data = r.json()
+
+
+#Title Year Director
+#Actors
+#imdbRating
+#Awards
+#Runtime
+print("*** Resultat från OMDB! ***")
+print(f"{data['Title']} ({data['Year']}) regisserades av {data['Director']} ")
+print(f"Skådisar: {data['Actors']}")
+print(f"IMDB betyg: {data['imdbRating']}")
+print(f"Awards: {data['Awards']}")
+print(f"Längd: {data['Runtime']}")
