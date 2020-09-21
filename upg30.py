84f12ed76a5418d813ce81e9fdbdf8b0ee7b8ca4
Daniel <daniel.yngve@student.kyh.se>
2020-09-21 14:05:27 +0200

    30,2

--- upg30.py
+++ upg30.py
@@ -1,3 +1,11 @@
-svar = input("Skriv in regnr: ")
+#30.1
+# svar = input("Skriv in regnr: ")
+# print(f"Bokstävsgrupp: {svar[0:3]}\n Siffergrupp: {svar[3:6]}")
 
-print(f"Bokstävsgrupp: {svar[0:3]}\n Siffergrupp: {svar[3:6]}")
+#30.2
+num = []
+tal = input("Ange tal med komma emellan: ").split(",")
+for i in tal:
+    num.append(int(i))
+print(f"Första talet: {tal[0]}\nSista talet: {tal[-1]}\nSumman av talen: {sum(num)}")
+print("Talen baklänges",",".join(tal[::-1]))
