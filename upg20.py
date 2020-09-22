15363d8e073f47e28dcbb2909de3ff2dfe99b663
Daniel <daniel.yngve@student.kyh.se>
2020-09-22 10:12:08 +0200

    snake

--- upg20.py
+++ upg20.py
@@ -19,7 +19,7 @@ pygame.display.set_caption('Snake!')
 clock = pygame.time.Clock()
 
 snake_block = 10
-snake_speed = 25
+snake_speed = 20
 
 font_style = pygame.font.SysFont("bahnschrift", 25)
 score_font = pygame.font.SysFont("comicsansms", 35)
