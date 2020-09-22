16c6f7819bc1b4aa3ffc92c3f8a1e9a94b0077fe
Daniel <daniel.yngve@student.kyh.se>
2020-09-22 10:11:54 +0200

    done

--- upg29.py
+++ upg29.py
@@ -4,12 +4,10 @@ import webbrowser
 content = Path("upg29.html").read_text()
 r = requests.get("https://api.adviceslip.com/advice")
 advice = r.json()
-dict = advice["slip"]
-value = dict["advice"]
+dic = advice["slip"]
+value = dic["advice"]
 
 html = content.replace("QUOTE_TEXT", value)
 p = Path("goat_quote.html")
 p.write_text(html, encoding='utf8')
 webbrowser.open("goat_quote.html")
-
-
