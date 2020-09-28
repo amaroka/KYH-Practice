7f990bc82afad5d3a3bd5bbe5147347722194003
Daniel <daniel.yngve@student.kyh.se>
2020-09-28 13:04:05 +0200

    39.2

--- upg39.py
+++ upg39.py
@@ -1,9 +1,20 @@
-def max(a,b,c):
-    if a > b and a > c:
-        return a
-    if b > a and b > c:
-        return b
-    if c > a and c > b:
-        return c
-value = max(4,8,6)
-print(value)
+#39.1
+# def max(a,b,c):
+#     if a > b and a > c:
+#         return a
+#     if b > a and b > c:
+#         return b
+#     if c > a and c > b:
+#         return c
+# value = max(4,8,6)
+# print(value)
+
+#39.2
+def total():
+    numbers = [5,10,15,20]
+    count = 0
+    for i in numbers:
+        count += i
+    print(count)
+
+total()
