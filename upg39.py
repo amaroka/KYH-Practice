3a6c6abfb3da4f7927f3d44c052f9a7018fd1b2b
Daniel <daniel.yngve@student.kyh.se>
2020-09-28 13:31:26 +0200

    39.3

--- upg39.py
+++ upg39.py
@@ -10,11 +10,20 @@
 # print(value)
 
 #39.2
-def total():
-    numbers = [5,10,15,20]
-    count = 0
-    for i in numbers:
-        count += i
-    print(count)
+# def total():
+#     numbers = [5,10,15,20]
+#     count = 0
+#     for i in numbers:
+#         count += i
+#     print(count)
+#
+# total()
 
+#39.3
+def total():
+    num = [1,2,3,4,5]
+    produkt = 1
+    for x in num:
+        produkt *= x
+    print(produkt)
 total()
