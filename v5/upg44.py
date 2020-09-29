7f107b448660a29365ca3d84d3897e41abc79b6a
Daniel <daniel.yngve@student.kyh.se>
2020-09-29 14:01:58 +0200

    44.3

--- v5/upg44.py
+++ v5/upg44.py
@@ -9,11 +9,27 @@
 # info(t)
 #
 
-tal1 = input("Skriv in ett tal: ")
-tal2 = input("Skriv in ett tal: ")
+# tal1 = input("Skriv in ett tal: ")
+# tal2 = input("Skriv in ett tal: ")
+#
+#
+# def tal(a,b):
+#     (a,b) = (b,a)
+#     print(a,b)
+# tal(tal1,tal2)
 
 
-def tal(a,b):
-    (a,b) = (b,a)
-    print(a,b)
-tal(tal1,tal2)
+
+T1 = (1,2,3)
+L1 = list(T1)
+print(L1)
+L1.append(4)
+T1 = tuple(L1)
+print(T1)
+
+
+
+def lista(ls):
+    return tuple(ls)
+ls = [1,2,3]
+print(lista(ls))
