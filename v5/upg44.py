fedd0d453dfba7301754c28fef88f51299c4136d
Daniel <daniel.yngve@student.kyh.se>
2020-09-29 13:40:23 +0200

    44.2

--- v5/upg44.py
+++ v5/upg44.py
@@ -1,9 +1,19 @@
-name = input("Skriv in ett namn: ")
-age = input("Skriv in ålder: ")
+# name = input("Skriv in ett namn: ")
+# age = input("Skriv in ålder: ")
+#
+# t = (name, age)
+#
+# def info(t):
+#     print(t[0])
+#     print(t[1])
+# info(t)
+#
 
-t = (name, age)
+tal1 = input("Skriv in ett tal: ")
+tal2 = input("Skriv in ett tal: ")
 
-def info(t):
-    print(t[0])
-    print(t[1])
-info(t)
+
+def tal(a,b):
+    (a,b) = (b,a)
+    print(a,b)
+tal(tal1,tal2)
